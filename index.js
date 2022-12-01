a530a1c5f1bf745cb66c87b4127b1f84ced95393
RMwebservices <rodrigomreidenbach@gmail.com>
2022-11-30 22:19:58 -0300

    fixing

--- index.js
+++ index.js
@@ -90,9 +90,9 @@ io.on('connection',async (socket)=>{
       
             }
 
-     io.sockets.emit('surveysvotes',{minafacilvotes,flockpoolvotes,raptoreumzonevotes,raptorhashvotes,inodezvotes,sullynodevotes,fastvotes,slowvotes,bitvotes,charlievotes,verdadvotes,rabidvotes,zlatachanvotes,verdadchanvotes,yesvotes,novotes})
+ 
         }
-  
+        io.sockets.emit('surveysvotes',{minafacilvotes,flockpoolvotes,raptoreumzonevotes,raptorhashvotes,inodezvotes,sullynodevotes,fastvotes,slowvotes,bitvotes,charlievotes,verdadvotes,rabidvotes,zlatachanvotes,verdadchanvotes,yesvotes,novotes})
     }
         
 
